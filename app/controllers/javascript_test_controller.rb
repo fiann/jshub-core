a4aed19e71760a4344a1202878bbdb4183550ee4
fianno <fianno@jshub.org>
2009-07-15 15:15:07 +0100

    Remove caching from test files

--- app/controllers/javascript_test_controller.rb
+++ app/controllers/javascript_test_controller.rb
@@ -1,16 +1,8 @@
-require "pathname"
-# Serve the JS test files as static html
-# Inspired by http://snafu.diarrhea.ch/blog/article/4-serving-static-content-with-rails
-
 class JavascriptTestController < ApplicationController
 
   # tag generates form submission to data_capture and doesn't know about
   # authenticity token 
   protect_from_forgery :except => :data_capture
-
-  NO_CACHE = [
-    'static/about/website',
-  ]
   
   # GET /test/unit/*
   # GET /test/unit/*?view=min
@@ -39,13 +31,13 @@ class JavascriptTestController < ApplicationController
       @files = children.reject{|f| f.directory?}
       render :template => 'javascript_unit_test/index', :layout => 'js'
     
-    elsif template_exists? path = "#{path}.html.erb"
+    elsif FileTest.file? template = "#{path}.html.erb"
       layout = (params[:view] == 'min') ? 'javascript_test_minimal' : true
-      render_cached path, layout
+      render :template => template, :layout => layout
 
     else
       raise ::ActionController::RoutingError,
-            "Recognition failed for #{path.inspect}"
+            "Recognition failed for #{path.relative_path_from(Pathname.new(RAILS_ROOT))}"
     end
   end
   
@@ -62,32 +54,6 @@ class JavascriptTestController < ApplicationController
       # Retrieve previous data from session
       @data = session['unit_test_data_'+@key] || '[none]'
       render :text => @data.to_json()
-    end
-  end
-
-  
-private
-
-# Define template_exists? as it is deprecated in Rails 2.3
-# ref. comments in original article and http://railsforum.com/viewtopic.php?id=28135
-unless ActionController::Base.private_instance_methods.include? 'template_exists?' 
-  def template_exists?(path)
-    self.view_paths.find_template(path, response.template.template_format) 
-  rescue ActionView::MissingTemplate 
-    false 
-  end
-end
-
-  def render_cached(path, layout=true)
-    if NO_CACHE.include? path
-      render :template => path
-    else
-      key = path.gsub('/', '-')
-      unless content = read_fragment(key)
-        content = render_to_string :template => path, :layout => false
-        write_fragment(key, content)
-      end
-      render :text => content, :layout => layout
     end
   end
 end
