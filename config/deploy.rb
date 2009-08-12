188293958f5d734ccd958ca8340ff79d952f39f8
Liam Clancy <liamc@jshub.org>
2009-08-12 17:10:25 +0100

    Rename Capistrano deploy name to match website prefix

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,29 @@
+# TODO
+# If you have previously been relying upon the code to start, stop 
+# and restart your mongrel application, or if you rely on the database
+# migration code, please uncomment the lines you require below
+
+# If you are deploying a rails app you probably need these:
+
+# load 'ext/rails-database-migrations.rb'
+# load 'ext/rails-shared-directories.rb'
+
+# There are also new utility libaries shipped with the core these 
+# include the following, please see individual files for more
+# documentation, or run `cap -vT` with the following lines commented
+# out to see what they make available.
+
+# load 'ext/spinner.rb'              # Designed for use with script/spin
+# load 'ext/passenger-mod-rails.rb'  # Restart task for use with mod_rails
+# load 'ext/web-disable-enable.rb'   # Gives you web:disable and web:enable
+
 # Enable multistage deploys
 set :stages, %w(staging production)
 set :default_stage, "staging"
 require 'capistrano/ext/multistage'
 
 # name of the application
-set :application, "akita-on-rails"
+set :application, "core"
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
