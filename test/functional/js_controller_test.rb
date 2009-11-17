3be6c9b64a79bd225efbb2240917b65b52dd8656
Liam Clancy (metafeather) <metafeather@mac.com>
2009-11-17 15:58:00 +0000

    Fix functional tests dependent on old codebase

--- test/functional/js_controller_test.rb
+++ test/functional/js_controller_test.rb
@@ -8,23 +8,23 @@ class JsControllerTest < ActionController::TestCase
     assert_equal "text/html", @response.content_type
     # check for presence of an example file and folder link
     assert_select "ul" do
-      assert_select "li a[href$=hub.js]", "hub.js"
+      assert_select "li a[href$=modules]", "modules"
       assert_select "li a[href$=data-capture]", "data-capture"
     end
   end
   
   test "should get raw source file" do
-    assert_routing "js/src/hub.js", { :controller => 'js', 
-      :action => 'src', :path => [ 'hub.js' ] }
-    get :src, { :path => [ 'hub.js' ] }
+    assert_routing "js/src/modules/hub/hub.js", { :controller => 'js', 
+      :action => 'src', :path => [ 'modules', 'hub', 'hub.js' ] }
+    get :src, { :path => [ 'modules', 'hub', 'hub.js' ] }
     assert_response :success
     assert_equal "text/javascript", @response.content_type
   end
   
   test "should get html formatted source file" do
-    assert_routing "js/show/hub.js", { :controller => 'js', 
-      :action => 'show', :path => [ 'hub.js' ] }
-    get :show, { :path => [ 'hub.js' ] }
+    assert_routing "js/show/modules/hub/hub.js", { :controller => 'js', 
+      :action => 'show', :path => [ 'modules', 'hub', 'hub.js' ] }
+    get :show, { :path => [ 'modules', 'hub', 'hub.js' ] }
     assert_response :success
     assert_equal "text/html", @response.content_type
   end
